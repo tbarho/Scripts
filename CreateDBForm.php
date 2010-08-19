c768c215d07e30ac6eb436483879841fdc63d675
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-19 23:12:33 +0000

    Added a paragraph so you know what the form is for

--- CreateDBForm.php
+++ CreateDBForm.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 	<form action="CreateDB.php" method="post">
+		<p>Enter a database name to add:</p>
 		<input type="text" id="DBName" name="DBName" />
 		<input type="submit" value="Submit" />
 	</form>
