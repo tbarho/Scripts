0ce705ad3ac3a2b0d1aaf3b1ff6ecd57a6ed4fab
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-19 23:08:09 +0000

    Form that has input for entering a DB name and connects to a script that adds the DB

---
+++ CreateDBForm.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Create a new Database via shell</title>
+</head>
+<body>
+	<form action="CreateDB.php" method="post">
+		<input type="text" id="DBName" name="DBName" />
+		<input type="submit" value="Submit" />
+	</form>
+</body>
+
+</html>
