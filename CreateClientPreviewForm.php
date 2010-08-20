f0f7bd3f4a980f1c9d87cc72b7dbadacac0cc8c0
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-20 15:45:32 +0000

    Form to create new sites

---
+++ CreateClientPreviewForm.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Create a new Database via shell</title>
+</head>
+<body>
+	<form action="CreateClientPreview.php" method="post">
+		<p>Enter a the name of the Preview you wish to add:</p>
+		<input type="text" id="PreviewName" name="PreviewName" />
+		<input type="submit" value="Submit" />
+	</form>
+</body>
+
+</html>
