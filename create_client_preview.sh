19074ab39680c8ff4b0a8a0d0b8a175bcd11f7b2
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-20 22:13:35 +0000

    Changed naming convention from BaseInstall2.4.1 to match svn, which is Core2.4.1

--- create_client_preview.sh
+++ create_client_preview.sh
@@ -21,23 +21,23 @@ echo "Creating the '$PREVIEW_NAME' directory..."
 mkdir ~/Sites/$PREVIEW_NAME
 echo "Done."
 
-echo "Copying required files from /BaseInstall2.4.1"
-cp -r ~/BaseInstall2.4.1/assets ~/Sites/$PREVIEW_NAME/assets 
-cp -r ~/BaseInstall2.4.1/mysite ~/Sites/$PREVIEW_NAME/mysite 
-cp -r ~/BaseInstall2.4.1/themes ~/Sites/$PREVIEW_NAME/themes 
-cp -r ~/BaseInstall2.4.1/.htaccess ~/Sites/$PREVIEW_NAME/.htaccess
+echo "Copying required files from /Core2.4.1"
+cp -r ~/Core2.4.1/trunk/assets ~/Sites/$PREVIEW_NAME/assets 
+cp -r ~/Core2.4.1/trunk/mysite ~/Sites/$PREVIEW_NAME/mysite 
+cp -r ~/Core2.4.1/trunk/themes ~/Sites/$PREVIEW_NAME/themes 
+cp -r ~/Core2.4.1/trunk/.htaccess ~/Sites/$PREVIEW_NAME/.htaccess
 echo "Done."
 
 echo "Symbolically linking the other necessary components from the core"
-ln -s ~/BaseInstall2.4.1/sapphire/ ~/Sites/$PREVIEW_NAME/sapphire 
-ln -s ~/BaseInstall2.4.1/cms/ ~/Sites/$PREVIEW_NAME/cms 
-ln -s ~/BaseInstall2.4.1/googlesitemaps/ ~/Sites/$PREVIEW_NAME/googlesitemaps 
-ln -s ~/BaseInstall2.4.1/index.php ~/Sites/$PREVIEW_NAME/index.php
+ln -s ~/Core2.4.1/trunk/sapphire/ ~/Sites/$PREVIEW_NAME/sapphire 
+ln -s ~/Core2.4.1/trunk/cms/ ~/Sites/$PREVIEW_NAME/cms 
+ln -s ~/Core2.4.1/trunk/googlesitemaps/ ~/Sites/$PREVIEW_NAME/googlesitemaps 
+ln -s ~/Core2.4.1/trunk/index.php ~/Sites/$PREVIEW_NAME/index.php
 echo "Done."
 
 echo "Changing the db in the _config.php file"
 rm ~/Sites/$PREVIEW_NAME/mysite/_config.php
-sed "s/{PREVIEW_NAME}/$PREVIEW_NAME/g" ~/BaseInstall2.4.1/mysite/_config.php > ~/Sites/$PREVIEW_NAME/mysite/_config.php
+sed "s/{PREVIEW_NAME}/$PREVIEW_NAME/g" ~/Core2.4.1/trunk/mysite/_config.php > ~/Sites/$PREVIEW_NAME/mysite/_config.php
 echo "Done."
 
 echo "Adding some module files via symlink."
