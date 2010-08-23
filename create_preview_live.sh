5ed95f3468311830c28a6ebaaba918844642e91e
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-23 21:07:09 +0000

    Testing code so far

--- create_preview_live.sh
+++ create_preview_live.sh
@@ -24,30 +24,5 @@ mkdir /tempProjects/$PROJECT_ID/trunk
 
 
 
-#Copy the files from the latest built Core
-
-cp -r /var/www/Core2.4.1/assets /tempProjects/$PROJECT_ID/trunk/assets 
-cp -r /var/www/Core2.4.1/mysite /tempProjects/$PROJECT_ID/trunk/mysite 
-cp -r /var/www/Core2.4.1/themes /tempProjects/$PROJECT_ID/trunk/themes 
-cp -r /var/www/Core2.4.1/.htaccess /tempProjects/$PROJECT_ID/trunk/.htaccess
-
-
-
-#Set the database in the _config.php file with sed
-
-rm /tmpProjects/$PROJECT_ID/trunk/mysite/_config.php
-sed "s/{PREVIEW_NAME}/$PROJECT_ID/g" /var/www/Core2.4.1/mysite/_config.php > /tempProjects/$PROJECT_ID/trunk/mysite/_config.php
-
-
-
-#Import the new Project into SVN
-
-svn import /tempProjects/$PROJECT_ID file:///var/svn/$PROJECT_ID - "Initial Import of Client Project $PROJECT_ID"
-
-
-
-#Clean up the temp directory
-
-rm -rf /tempProjects/$PROJECT_ID
 
 
