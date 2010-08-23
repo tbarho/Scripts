9fc7a9e25b14e2c06d4ad1a7fb9b37bb69b28718
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-23 20:56:56 +0000

    Kind of a restart.  I think this is better

--- create_preview_live.sh
+++ create_preview_live.sh
@@ -13,54 +13,41 @@ if [ $# != 1 ]; then
     usage
 fi
 
-#TODO: When a client clicks order, to create a preview site
 
-#Create the repository
 
-	#Make temp directory and checkout core
-	mkdir /tmpprojects/$PROJECT_ID
-	svn co file:///var/svn/Core2.4.1 /tmpprojects/$PROJECT_ID
-	
-	#Remove unnecessary files / folders
-	rm -rf /tmpprojects/$PROJECT_ID/ChangeLog 
-	rm -rf /tmpprojects/$PROJECT_ID/cms
-	rm -rf /tmpprojects/$PROJECT_ID/COPYING
-	rm -rf /tmpprojects/$PROJECT_ID/favicon.ico 
-	rm -rf /tmpprojects/$PROJECT_ID/googlesitemaps/
-	rm -rf /tmpprojects/$PROJECT_ID/index.php 
-	rm -rf /tmpprojects/$PROJECT_ID/INSTALL 
-	rm -rf /tmpprojects/$PROJECT_ID/install.php 
-	rm -rf /tmpprojects/$PROJECT_ID/MakeFile
-	rm -rf /tmpprojects/$PROJECT_ID/sapphire/
-	rm -rf /tmpprojects/$PROJECT_ID/_ss_environment.php 
-	rm -rf /tmpprojects/$PROJECT_ID/UPGRADING 
-	rm -rf /tmpprojects/$PROJECT_ID/web.config 
-	
-	#SVN Import as new Repo
-	svn import /tmpprojects/$PROJECT_ID file:///var/svn/$PROJECT_ID -m "Initial Import - Client Preview $PROJECT_ID"
-	
-	#Delete the temp folder
-	rm -rf /tmpprojects/$PROJECT_ID
-	
-#Get the preview site working
-	
-	#Export the $PROJECT_ID to the web root as a new site
-	svn export file:///var/svn/$PROJECT_ID /var/www/html/$PROJECT_ID
-	
-	#Create the database
-	mysql -e"CREATE DATABASE $PROJECT_ID;" -u root -p"Redrooster8"
-	
-	#Edit the config to point to the new database
-	rm /var/www/html/$PROJECT_ID/mysite/_config.php
-	sed "s/{PREVIEW_NAME}/$PROJECT_ID/g" /var/www/Core2.4.1/mysite/_config.php > /var/www/html/$PROJECT_ID/mysite/_config.php
-	
-	#Add the symbolic links to the working core
-	ln -s /var/www/Core2.4.1/sapphire/ /var/www/html/$PROJECT_ID/sapphire 
-	ln -s /var/www/Core2.4.1/cms/ /var/www/html/$PROJECT_ID/cms 
-	ln -s /var/www/Core2.4.1/googlesitemaps/ /var/www/html/$PROJECT_ID/googlesitemaps 
-	ln -s /var/www/Core2.4.1/index.php /var/www/html/$PROJECT_ID/index.php
-	
-	#Run a wget to build the database
-	/usr/bin/wget -O /var/www/html/"$PROJECT_ID"/build-verification.html http://www.sitesprocket.com/"$PROJECT_ID"/dev/build?flush=1 2>&1
+#Build the scaffolding for the repo
+
+mkdir /tempProjects/$PROJECT_ID
+mkdir /tempProjects/$PROJECT_ID/branches
+mkdir /tempProjects/$PROJECT_ID/tags
+mkdir /tempProjects/$PROJECT_ID/trunk
+
+
+
+#Copy the files from the latest built Core
+
+cp -r /var/www/Core2.4.1/assets /tempProjects/$PROJECT_ID/trunk/assets 
+cp -r /var/www/Core2.4.1/mysite /tempProjects/$PROJECT_ID/trunk/mysite 
+cp -r /var/www/Core2.4.1/themes /tempProjects/$PROJECT_ID/trunk/themes 
+cp -r /var/www/Core2.4.1/.htaccess /tempProjects/$PROJECT_ID/trunk/.htaccess
+
+
+
+#Set the database in the _config.php file with sed
+
+rm /tmpProjects/$PROJECT_ID/trunk/mysite/_config.php
+sed "s/{PREVIEW_NAME}/$PROJECT_ID/g" /var/www/Core2.4.1/mysite/_config.php > /tempProjects/$PROJECT_ID/trunk/mysite/_config.php
+
+
+
+#Import the new Project into SVN
+
+svn import /tempProjects/$PROJECT_ID file:///var/svn/$PROJECT_ID
+
+
+
+#Clean up the temp directory
+
+rm -rf /tempProjects/$PROJECT_ID
 
 
