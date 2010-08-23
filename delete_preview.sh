80a0eea8d7732ded20408c971e715dcad499cc66
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-23 21:38:26 +0000

    Drop DB for test projects

--- delete_preview.sh
+++ delete_preview.sh
@@ -14,10 +14,9 @@ if [ $# != 1 ]; then
 fi
 
 
-
-#Delete the temp site
-rm -rf /tempProjects/$PROJECT_ID
-
 #Remove the project from SVN
 svn delete file:///var/svn/$PROJECT_ID -m "Deleting $PROJECT_ID"
+
+#Remove the database that was created
+mysql -e"DROP DATABASE IF EXISTS $PROJECT_ID;" -u root -p"Redrooster8"
 
