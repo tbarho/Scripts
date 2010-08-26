1285df9178218c9d87adddfff352efb39c287c99
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-26 16:29:35 +0000

    Fixed if then statement Added exit; command

--- checkExist_www.sh
+++ checkExist_www.sh
@@ -13,8 +13,9 @@ if [ $# != 1 ]; then
     usage
 fi
 
-if [ -e "/var/www/sites/$PROJECT_ID" ]
+if [ -e "/var/www/sites/$PROJECT_ID" ]; then
 	echo "There is already a directory named $PROJECT_ID in /var/www/sites.  Exiting..."
+	exit;
 else
 	echo "$PROJECT_ID does not exist in /var/www/sites.  Proceeding..."
 fi
