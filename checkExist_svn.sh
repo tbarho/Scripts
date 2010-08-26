82d82548a40cb8965d8fe4d15c8b1c9673563fe9
root <root@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-26 07:34:22 +0000

    initial check in

---
+++ checkExist_svn.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+
+PROJECT_ID=$1
+
+echo "'PROJECT_ID=$PROJECT_ID'"
+
+function usage {
+    echo "$0  [project id]";
+    exit 1;
+}
+
+if [ $# != 1 ]; then
+    usage
+fi
+
+svn ls file:///var/svn/$PROJECT_ID &> /dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+	echo "SVN repo for $PROJECT_ID already exists. Exiting..."
+	exit;
+else
+	echo "SVN repo for $PROJECT_ID does notexists. Proceeding..."
+fi
+
+
