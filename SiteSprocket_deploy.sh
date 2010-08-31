a4e6723ee98c9f466cc43c86bbaaba020d40403a
root <root@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-08-31 06:24:07 +0000

    IMPORT - SiteSprocket Deployment script

---
+++ SiteSprocket_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#ToDo:
+
+#Delete the existing web root
+
+#Put up "Under Construction Page"
+
+#Dump existing database to named file (with date)
+
+#Export the current build to the web root
+
+#Run a wget to rebuild database; return success / fail
+
+
+
