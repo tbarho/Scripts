a8ca346ac0a2e07eee068856d9e179d6fbed9816
root <root@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-11-17 22:49:56 +0000

    FIX: Changed svn directory

--- clientCreate.sh
+++ clientCreate.sh
@@ -22,37 +22,37 @@ fi
 
 #Build the scaffolding for the repo
 
-mkdir /tempProjects/$PROJECT_ID
-mkdir /tempProjects/$PROJECT_ID/branches
-mkdir /tempProjects/$PROJECT_ID/tags
-mkdir /tempProjects/$PROJECT_ID/trunk
+mkdir /tmp/tmp_$PROJECT_ID
+mkdir /tmp/tmp_$PROJECT_ID/branches
+mkdir /tmp/tmp_$PROJECT_ID/tags
+mkdir /tmp/tmp_$PROJECT_ID/trunk
 
 
 
 #Copy the files from the latest built Core
 
-cp -r /var/www/Core2.4.1/assets /tempProjects/$PROJECT_ID/trunk/assets 
-cp -r /var/www/Core2.4.1/mysite /tempProjects/$PROJECT_ID/trunk/mysite 
-cp -r /var/www/Core2.4.1/themes /tempProjects/$PROJECT_ID/trunk/themes 
-cp -r /var/www/Core2.4.1/.htaccess /tempProjects/$PROJECT_ID/trunk/.htaccess
+cp -r /var/www/Core2.4.1/assets /tmp/tmp_$PROJECT_ID/trunk/assets 
+cp -r /var/www/Core2.4.1/mysite /tmp/tmp_$PROJECT_ID/trunk/mysite 
+cp -r /var/www/Core2.4.1/themes /tmp/tmp_$PROJECT_ID/trunk/themes 
+cp -r /var/www/Core2.4.1/.htaccess /tmp/tmp_$PROJECT_ID/trunk/.htaccess
 
 
 
 #Set the database in the _config.php file with sed
 
-rm /tempProjects/$PROJECT_ID/trunk/mysite/_config.php
-sed "s/{PREVIEW_NAME}/$PROJECT_ID/g" /var/www/Core2.4.1/mysite/_config.php > /tempProjects/$PROJECT_ID/trunk/mysite/_config.php
+rm /tmp/tmp_$PROJECT_ID/trunk/mysite/_config.php
+sed "s/{PREVIEW_NAME}/$PROJECT_ID/g" /var/www/Core2.4.1/mysite/_config.php > /tmp/tmp_$PROJECT_ID/trunk/mysite/_config.php
 
 
 
 #Import the new Project into SVN
 
-svn import /tempProjects/$PROJECT_ID file:///var/svn/$PROJECT_ID -m "Initial Import of Client Project $PROJECT_ID"
+svn import /tmp/tmp_$PROJECT_ID file:///var/svn/client_sites/$PROJECT_ID -m "Initial Import of Client Project $PROJECT_ID"
 
 
 #Delete the temp directory
 
-rm -rf /tempProjects/$PROJECT_ID
+rm -rf /tmp/tmp_$PROJECT_ID
 
 
 
@@ -67,7 +67,7 @@ mysql -e"CREATE DATABASE $PROJECT_ID;" -u root -p"Redrooster8"
 
 #Export the project from SVN to the web root
 
-svn export file:///var/svn/$PROJECT_ID/trunk /var/www/sites/$PROJECT_ID
+svn export file:///var/svn/client_sites/$PROJECT_ID/trunk /var/www/sites/$PROJECT_ID
 
 
 
